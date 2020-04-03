e542d8c09649c340d30c20434b9742a63593a596
majestic2110 <majestic2110@gmail.com>
2020-04-03 21:44:16 +0530

    Longest subtring without repeating characters

---
+++ mission-amz/src/com/amz/problems/IP_G_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,35 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IP_G_LongestSubstringWithoutRepeatingCharacters extends AbstractQuestion {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int start = 0;
+        int end = 0;
+        for (end = 0; end < s.length(); end++) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(start, map.get(s.charAt(end)) + 1);
+            }
+            maxLen = Math.max(maxLen, end - start + 1);
+            map.put(s.charAt(end), end);
+        }
+        return maxLen;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        IP_G_LongestSubstringWithoutRepeatingCharacters test = new IP_G_LongestSubstringWithoutRepeatingCharacters();
+        String str = "abcbbshdge";
+        System.out.println("Length of the longest substring := " + test.lengthOfLongestSubstring(str));
+    }
+}
