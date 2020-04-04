4198b920215da91498d6872a7e8632b1584b15d6
majestic2110 <majestic2110@gmail.com>
2020-04-04 13:13:17 +0530

    height checker, long pressed name

---
+++ mission-amz/src/com/amz/problems/RS_G01_HeightChecker.java
@@ -0,0 +1,31 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class RS_G01_HeightChecker extends AbstractQuestion {
+    public int heightChecker(int[] heights) {
+        if (heights == null || heights.length <= 1) {
+            return 0;
+        }
+        int[] copy = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(copy);
+        int count = 0;
+        for (int i = 0; i < copy.length; i++) {
+            if (copy[i] != heights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        RS_G01_HeightChecker test = new RS_G01_HeightChecker();
+        int[] tree = {3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
+        System.out.println("No. of Students required to change places := " + test.heightChecker(tree));
+    }
+}
---
+++ mission-amz/src/com/amz/problems/RS_G01_LongPressedName.java
@@ -0,0 +1,39 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+public class RS_G01_LongPressedName extends AbstractQuestion {
+    public boolean isLongPressedName(String name, String typed) {
+        if (name == null || typed == null || name.length() == 0 || typed.length() == 0) {
+            return false;
+        }
+        if (name.charAt(0) != typed.charAt(0) || typed.length() < name.length()) {
+            return false;
+        }
+        int i = 1;
+        int t = 1;
+        char prev = name.charAt(0);
+        while (t < typed.length()) {
+            if (i < name.length() && typed.charAt(t) == name.charAt(i)) {
+                prev = name.charAt(i);
+                i++;
+                t++;
+            } else if (typed.charAt(t) == prev) {
+                t++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        RS_G01_LongPressedName test = new RS_G01_LongPressedName();
+        String name = "saeed";
+        String typed = "ssaaedd";
+        System.out.println("Is Long pressed name := " + test.isLongPressedName(name, typed));
+    }
+}
