3d6dc2662049631feccd40d920418ba4ba0e6964
majestic2110 <majestic2110@gmail.com>
2020-04-06 18:50:17 +0530

    group anagrams

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_GroupAnagrams.java
@@ -0,0 +1,40 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+import java.util.*;
+
+public class CHZ_30Day_GroupAnagrams extends AbstractQuestion {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<String>> resList = new ArrayList<>();
+        Map<String, List<String>> mapStrs = new HashMap<>();
+        int[] map;
+        for (String item : strs) {
+            map = new int[26];
+            for (char ch : item.toCharArray()) {
+                map[ch - 'a']++;
+            }
+            String key = Arrays.toString(map);
+            List<String> temp = mapStrs.getOrDefault(key, new ArrayList<>());
+            temp.add(item);
+            mapStrs.put(key, temp);
+        }
+        for (String key : mapStrs.keySet()) {
+            resList.add(mapStrs.get(key));
+        }
+        return resList;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        CHZ_30Day_GroupAnagrams test = new CHZ_30Day_GroupAnagrams();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("Group Anagrams := " + test.groupAnagrams(strs));
+    }
+}
