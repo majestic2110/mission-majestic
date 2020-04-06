02ddcdcaa38880e2706f57f4c21e14d6fd3351ef
majestic2110 <majestic2110@gmail.com>
2020-04-06 18:51:06 +0530

    group anagrams

--- mission-amz/src/com/amz/problems/CHZ_30Day_GroupAnagrams.java
+++ mission-amz/src/com/amz/problems/CHZ_30Day_GroupAnagrams.java
@@ -19,6 +19,7 @@ public class CHZ_30Day_GroupAnagrams extends AbstractQuestion {
             for (char ch : item.toCharArray()) {
                 map[ch - 'a']++;
             }
+            // can be replaced by sort, or any other encoding method
             String key = Arrays.toString(map);
             List<String> temp = mapStrs.getOrDefault(key, new ArrayList<>());
             temp.add(item);
