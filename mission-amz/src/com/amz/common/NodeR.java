a5e3eda7e7287367141eb0391140c345ab747109
majestic2110 <majestic2110@gmail.com>
2020-04-08 18:02:31 +0530

    copy list with random pointer

---
+++ mission-amz/src/com/amz/common/NodeR.java
@@ -0,0 +1,13 @@
+package com.amz.common;
+
+public class NodeR {
+    public int val;
+    public NodeR next;
+    public NodeR random;
+
+    public NodeR(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
---
+++ mission-amz/src/com/amz/problems/IP_G_CopyListwithRandomPointer.java
@@ -0,0 +1,35 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import com.amz.common.NodeR;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class IP_G_CopyListwithRandomPointer extends AbstractQuestion {
+    public NodeR copyRandomList(NodeR head) {
+        if (head == null) {
+            return null;
+        }
+        HashMap<NodeR, NodeR> map = new HashMap<>();
+        NodeR temp = head;
+        while (temp != null) {
+            map.put(temp, new NodeR(temp.val));
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp != null) {
+            NodeR newNodeR = map.get(temp);
+            newNodeR.next = map.get(temp.next);
+            newNodeR.random = map.get(temp.random);
+            temp = temp.next;
+        }
+        return map.get(head);
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        // to be written
+    }
+}
