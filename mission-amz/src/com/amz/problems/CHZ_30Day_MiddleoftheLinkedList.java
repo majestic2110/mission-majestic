cbb7236a6cd43c5e480bb5a90e1845e7b4b1d755
majestic2110 <majestic2110@gmail.com>
2020-04-08 17:09:41 +0530

    middle of the linked list

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_MiddleoftheLinkedList.java
@@ -0,0 +1,35 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import com.amz.common.ListNode;
+import org.junit.Test;
+
+public class CHZ_30Day_MiddleoftheLinkedList extends AbstractQuestion {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        CHZ_30Day_MiddleoftheLinkedList test = new CHZ_30Day_MiddleoftheLinkedList();
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(5);
+        ListNode middle = middleNode(l1);
+        System.out.print("l1 := ");
+        l1.display();
+        System.out.print("Middle of the linked list := ");
+        middle.display();
+    }
+}
