f38c6a559d6e4049c547ac325c9a4cf5c70739e2
majestic2110 <majestic2110@gmail.com>
2020-04-08 17:33:59 +0530

    remove nth node from linked list

---
+++ mission-amz/src/com/amz/problems/IP_G_RemoveNthNodeFromEndofList.java
@@ -0,0 +1,44 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import com.amz.common.ListNode;
+import org.junit.Test;
+
+public class IP_G_RemoveNthNodeFromEndofList extends AbstractQuestion {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        int k = n;
+        ListNode first = dummyHead;
+        ListNode second = dummyHead;
+        while (k-- >= 0) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummyHead.next;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        IP_G_RemoveNthNodeFromEndofList test = new IP_G_RemoveNthNodeFromEndofList();
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(5);
+        l1.next.next.next.next = new ListNode(6);
+        System.out.print("l1 := ");
+        l1.display();
+        int n = 2;
+        ListNode mod = removeNthFromEnd(l1, n);
+        System.out.print("removed " + n + " from last := ");
+        mod.display();
+    }
+}
