d244b19dea58a2d82334cf889c88ebed1a90ae43
majestic2110 <majestic2110@gmail.com>
2020-04-12 15:27:08 +0530

    last stone weight

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_LastStoneWeight.java
@@ -0,0 +1,36 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class CHZ_30Day_LastStoneWeight extends AbstractQuestion {
+    public int lastStoneWeight(int[] stones) {
+        if (stones == null || stones.length == 0) {
+            return 0;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+        while (pq.size() > 1) {
+            int x = pq.poll();
+            int y = pq.poll();
+            if (x != y) {
+                pq.offer(x - y);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        CHZ_30Day_LastStoneWeight test = new CHZ_30Day_LastStoneWeight();
+        int[] stones = {2, 7, 4, 1, 8, 1};
+        System.out.println("Last Stone := " + test.lastStoneWeight(stones));
+    }
+}
