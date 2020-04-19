ca6bb26eb0ac051ba3b17e004c5cd5b764104925
majestic2110 <majestic2110@gmail.com>
2020-04-19 16:10:35 +0530

    minimum path sum

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_MinimumPathSum.java
@@ -0,0 +1,31 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+public class CHZ_30Day_MinimumPathSum extends AbstractQuestion {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        int dp[] = new int[grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0) {
+                    dp[j] = grid[i][j] + (j > 0 ? dp[j - 1] : 0);
+                } else {
+                    dp[j] = grid[i][j] + Math.min(dp[j], j > 0 ? dp[j - 1] : Integer.MAX_VALUE);
+                }
+            }
+        }
+        return dp[grid[0].length - 1];
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        CHZ_30Day_MinimumPathSum test = new CHZ_30Day_MinimumPathSum();
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println("Min path := " + test.minPathSum(grid));
+    }
+}
