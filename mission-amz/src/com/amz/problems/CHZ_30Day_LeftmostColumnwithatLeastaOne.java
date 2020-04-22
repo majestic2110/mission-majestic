23d88c63e5a3e68a77bce308bb976d47d71f0990
majestic2110 <majestic2110@gmail.com>
2020-04-22 19:16:03 +0530

    binary matrix sorted rows, left most column with 1

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_LeftmostColumnwithatLeastaOne.java
@@ -0,0 +1,40 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CHZ_30Day_LeftmostColumnwithatLeastaOne extends AbstractQuestion {
+
+    interface BinaryMatrix {
+        public int get(int x, int y);
+
+        public List<Integer> dimensions();
+    }
+
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        if (binaryMatrix == null) {
+            return 0;
+        }
+        List<Integer> dims = binaryMatrix.dimensions();
+        int colN = -1;
+        int row = 0;
+        int col = dims.get(1) - 1;
+        while (row < dims.get(0) && col >= 0) {
+            if (binaryMatrix.get(row, col) == 1) {
+                colN = col;
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return colN;
+    }
+
+    @Override
+    @Test
+    public void evaluate() {
+        // to be done
+    }
+}
