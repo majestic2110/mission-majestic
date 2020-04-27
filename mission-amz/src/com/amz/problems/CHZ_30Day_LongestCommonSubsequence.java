aa958c99a319a40c1a267c11bce4dfd7161273d9
majestic2110 <majestic2110@gmail.com>
2020-04-27 15:56:33 +0530

    longest common subsequence, maximal square

---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_LongestCommonSubsequence.java
@@ -0,0 +1,31 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+
+public class CHZ_30Day_LongestCommonSubsequence extends AbstractQuestion {
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
+            return 0;
+        }
+        text1 = '#' + text1;
+        text2 = '#' + text2;
+        int[][] dp = new int[text1.length()][text2.length()];
+        int maxLCS = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(Math.max(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                }
+                maxLCS = Math.max(maxLCS, dp[i][j]);
+            }
+        }
+        return maxLCS;
+    }
+
+    @Override
+    public void simulate() {
+
+    }
+}
---
+++ mission-amz/src/com/amz/problems/CHZ_30Day_MaximalSquare.java
@@ -0,0 +1,29 @@
+package com.amz.problems;
+
+import com.amz.common.AbstractQuestion;
+
+public class CHZ_30Day_MaximalSquare extends AbstractQuestion {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int maxOne = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                } else if (matrix[i][j] == '1') {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+                maxOne = Math.max(dp[i][j], maxOne);
+            }
+        }
+        return maxOne * maxOne;
+    }
+
+    @Override
+    public void simulate() {
+        // to be done
+    }
+}
